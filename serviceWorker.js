749d20aa7bd9387929bedd6dc65a13fc4e2aeb65
eli.m <ejmora7@gmail.com>
2019-03-04 18:57:27 -0800

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,1 +1,31 @@
 
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/css/styles.css",
+        "/images/append.jpg",
+        "/images/ascending.png",
+        "/images/brand.png",
+        "/images/buildingblocks.png",
+        "/images/burger.jpg",
+        "/images/calculator.jpg",
+        "/images/calendar.jpeg",
+        "/images/detect.jpg",
+        "/images/dynamicbrail.jpg",
+        "/images/flicker.jpg",
+        "/images/hooks.jpg",
+        "/images/ifelse.jpg",
+        "/images/money.jpg",
+        "/images/myphoto.jpg",
+        "/images/restaurant.jpg",
+        "/images/storage.jpg",
+        "/images/video1.jpg",
+        "/images/zipcodeCity.jpeg",
+        "/js/scripts.js"
+      ]);
+    })
+  );
+});
